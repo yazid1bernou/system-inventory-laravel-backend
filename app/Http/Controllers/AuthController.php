163c2c9da66b61111cadb31dda16a0250a748dbb
unknown <yazid1bernou@gmail.com>
2023-08-23 13:52:01 +0000

    create function of login in AuthController

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -12,6 +12,13 @@ class AuthController extends Controller
         // Function Login
         public function login (AuthRequest $request)
          {        
-            $user =  getUserByEmailOrPhone($request);
+            $user =  (new User())->getUserByEmailOrPhone($request->all()); 
+            if ($user && Hash::check($request->input('password') , $user->password)) {
+               $user_data['token'] = $user->createToken($user->email)->plainTextToken ; 
+               $user['name'] = $user->name ;
+               $user['phone'] = $user->phoen ;
+               $user['email'] = $user->email ;
+               $user['photo'] = $user->photo ;
+            }
          }
 }
